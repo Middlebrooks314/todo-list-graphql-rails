426e76c1baae373ebf507f5d4b6a0377a9e2a818
Malisa Middlebrooks <middlebrooks314@gmail.com>
2021-08-03 21:55:05 -0500

    Add production route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: 'graphiql', graphql_path: "graphql#execute"
   end
+  if Rails.env.production?
+    mount GraphiQL::Rails::Engine, at: 'graphiql', graphql_path: "graphql#execute"
+  end
   
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

445045d9c933a8f38b72b4cc12009356748fe389
Malisa Middlebrooks <middlebrooks314@gmail.com>
2021-08-03 21:50:56 -0500

    change precompile back to development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  # config.assets.precompile += ['graphiql/rails/application.js', 'graphiql/rails/application.css']
+  config.assets.precompile += ['graphiql/rails/application.js', 'graphiql/rails/application.css']
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true

b5fd2c1064238968a63a87c02ee892e95d155f15
Malisa Middlebrooks <middlebrooks314@gmail.com>
2021-08-03 22:03:31 -0500

    Change to or statement

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: 'graphiql', graphql_path: "graphql#execute"
-  end
-  if Rails.env.production?
+  if Rails.env.development? || Rails.env.production?
     mount GraphiQL::Rails::Engine, at: 'graphiql', graphql_path: "graphql#execute"
   end
   

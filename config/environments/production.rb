b262c1e1d4908289d1115c0aa95873905978d4b9
Malisa Middlebrooks <middlebrooks314@gmail.com>
2021-08-03 21:47:12 -0500

    update precompile to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.assets.precompile += ['graphiql/rails/application.js', 'graphiql/rails/application.css']
+  config.assets.precompile += ['graphiql/rails/application.js', 'graphiql/rails/application.css']
 
  
 

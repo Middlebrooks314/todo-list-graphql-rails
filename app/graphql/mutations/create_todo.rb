94bb8ebff429ef5949dd24cecd745af1d45d6861
Malisa Middlebrooks <middlebrooks314@gmail.com>
2021-08-06 16:53:57 -0500

    Add default values for description and completed fields

--- app/graphql/mutations/create_todo.rb
+++ app/graphql/mutations/create_todo.rb
@@ -2,13 +2,13 @@ class Mutations::CreateTodo < Mutations::BaseMutation
   null true
   argument :title, String, required: true
   argument :description, String, required: false
-  argument :completed, Boolean, required: true
+  argument :completed, Boolean, required: false
   argument :author, String, required: true
 
   field :todo, Types::TodoType, null: false
   field :errors, [String], null: false
 
-  def resolve(title:, description:, completed:, author:)
+  def resolve(title:, description: nil, completed: false, author:)
     todo = Todo.new(title: title, description: description, completed: completed, author: author)
     if todo.save
       {

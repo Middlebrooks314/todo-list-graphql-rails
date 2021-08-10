ee8b8a849865fe232332fa31ddbd138909330657
Malisa Middlebrooks <middlebrooks314@gmail.com>
2021-08-10 14:18:05 -0500

    Add destroy todo mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,7 +2,11 @@ module Types
   class MutationType < Types::BaseObject
     field :create_todo, mutation: Mutations::CreateTodo,
      description: "Adds new todo"
+
     field :update_todo, mutation: Mutations::UpdateTodo,
      description: "Updates a todo"
+
+    field :destroy_todo, mutation:Mutations::DestroyTodo,
+      description: "Removes a todo"
   end
 end 
